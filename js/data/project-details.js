cd337d75e5e8790d18ae72b6f39f36f8192460d0
Cesar <mail.mansilla@gmail.com>
2025-04-15 10:28:42 -0300

    Portfolio data

---
+++ js/data/project-details.js
@@ -0,0 +1,58 @@
+const projectDetails = {
+  "havanna-ecommerce": {
+    "client": "Havanna",
+    "project": "Ecommerce",
+    "date": "2020-05",
+    "image_cover": "havanna-ecommerce.jpg",
+    "image_main": "havanna-ecommerce-main.jpg",
+    "image_big": "havanna-ecommerce-big-image.jpg",
+    "excerpt": "Desarrollo de ecommerce & integraciones via API",
+    "role": "Planificación + Desarrollo",
+    "overview": "Ecommerce con integración con ERP, sistema de facturación y envío.",
+    "requirement": "Planificación por etapas del montaje de un ecommerce que les permita entender el alcance de esta pata digital.",
+    "development": "<p class=\"mb-4\">Montaje de la tienda bajo la plataforma <strong>Woocommerce</strong> e integración con sistemas externos a través de plugins y consumos de APIs.</p><p class=\"mb-4\">Desarrollo de funcionalidades personalizadas, como ser un sistema de consumos con <strong>gift cards</strong> y la integración del <strong>Botón de arrepentimiento</strong> según resolución 424/2020.</p><p class=\"mb-4\">Planteamiento de diversas soluciones a <strong>nuevos requerimientos</strong> que van surgiendo a medida que el desarrollo continúa creciendo.</p>",
+    "link": "https://tienda.havanna.com.ar"
+  },
+  "havanna-ddm-2017": {
+    "client": "Havanna",
+    "project": "Día de la madre 2017",
+    "date": "2017-10",
+    "image_cover": "havanna-ddm-2017.jpg",
+    "image_main": "havanna-ddm-2017-main.jpg",
+    "image_big": "havanna-ddm-2017-big-image.jpg",
+    "excerpt": "Día de la madre 2017",
+    "role": "Idea + Planificación + Desarrollo",
+    "overview": "Aplicación para que la gente pueda personalizar una edición limitada de cajas de chocolates que llegaron a casa de sus madres en su día.",
+    "requirement": "Con motivo del día de la madre querían entregar una <strong>experiencia única</strong> y personalizada. Contaban con la posiblidad de imprimir 52000 cajas con distintos motivos predefinidos y otras 50 con <strong>diseños totalmente personalizados</strong>.",
+    "development": "<p class=\"mb-4\">Desarrollamos un hotsite que guiaba en el proceso de <strong>personalización de cada una de las caras</strong>, era posible subir fotos, cargar mensajes, aplicar stickers y elegir colores.</p><p class=\"mb-4\">En el último paso se podría crear un mensaje personalizado, ya sea de texto o en video. Esta última opción grababa en el momento y lo editaba on the fly en un formato prestablecido que contaba una mini historia en unos 15 segundos. Luego, era <strong>subido automáticamente a Youtube</strong>.</p><p class=\"mb-4\">Este video sólo estaba disponible a partir del día de la madre a través del escaneo de un <strong>código QR</strong>.</p>",
+    "link": false
+  },
+  "budweiser-made-for-music": {
+    "client": "Budweiser",
+    "project": "Made for Music 2017",
+    "date": "2017-02",
+    "image_cover": "budweiser-m4m-2017.jpg",
+    "image_main": "budweiser-m4m-2017-main.jpg",
+    "image_big": "budweiser-m4m-2017-big-image.jpg",
+    "excerpt": "Made for Music<br>Argentina 2017",
+    "role": "Planificación + Desarrollo",
+    "overview": "Hotsite, Webapp y desarrollo BTL para el evento Made for Music Argentina 2017",
+    "requirement": "Bandas regionales de distintas provincias deberían <strong>incribirse y competir</strong> por su lugar como teloneros de prestigiosas bandas.",
+    "development": "<p class=\"mb-4\">Este proyecto contó con 2 grandes etapas, la primera se trataba de un <strong>hotsite</strong> que permitía a los interesados inscribirse y comenzar a sumar votos para ganar su lugar en el recital.</p><p class=\"mb-4\">Esto fué potenciado por distintas campañas en redes sociales.</p><p class=\"mb-4\">La segunda etapa tenía lugar en el mismo momento y lugar del recital, dónde a través de posteos en redes con determinados hashtags <strong>el sistema obtenía los posteos de la gente</strong>, pasaban por un proceso de moderación, y los aprobados aparecian en enormes paneles ubicados en los laterales del escenario.</p>",
+    "link": false
+  },
+  "idem-cdi": {
+    "client": "Idem",
+    "project": "Catálogo digital",
+    "date": "2018-06",
+    "image_cover": "idem-cdi.jpg",
+    "image_main": "idem-cdi-main.jpg",
+    "image_big": "idem-cdi-big-image.jpg",
+    "excerpt": "Catálogo Digital",
+    "role": "Planificación + Desarrollo",
+    "overview": "APP híbrida para Android e iOS",
+    "requirement": "<strong>Rehacer la aplicación</strong> que tenían, porque era demasiado costoso hacer actualizaciones debido a su antigüedad.",
+    "development": "<p class=\"mb-4\">El desarrollo fué hecho con <strong>Ionic</strong>, el cuál está basado en <strong>AngularJS</strong> y permite construir aplicaciones híbridas.</p><p class=\"mb-4\">El backend fué desarrollado en <strong>PHP</strong> ya que parte del funcionamiento debió ser extendido a partir de un desarrollo ya realizado en dicho lenguaje.</p><p class=\"mb-4\">Se desarrolló un complejo sistema de descuentos que dependía del cliente, de los artículos a comprar y de diversas reglas que podían ser combinadas.</p>",
+    "link": "https://play.google.com/store/apps/details?id=com.dmfusion.idemmobile"
+  }
+}; 
